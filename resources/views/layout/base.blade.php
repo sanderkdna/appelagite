85bcc00e37b9b9d44f1401607e3cb04fce5fd241
Sander Cadena <sanderkdna@gmail.com>
2023-05-05 06:24:27 -0500

    update app meta

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -1,13 +1,4 @@
 <!DOCTYPE html>
-<!--
-Template Name: Rubick - HTML Admin Dashboard Template
-Author: Left4code
-Website: http://www.left4code.com/
-Contact: muhammadrizki@left4code.com
-Purchase: https://themeforest.net/user/left4code/portfolio
-Renew Support: https://themeforest.net/user/left4code/portfolio
-License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
--->
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="{{ $dark_mode ? 'dark' : '' }}{{ $color_scheme != 'default' ? ' ' . $color_scheme : '' }}">
 <!-- BEGIN: Head -->
 <head>
@@ -15,9 +6,9 @@ License: You must have a valid license purchased only from themeforest(the above
     <link href="{{ asset('dist/images/logo.svg') }}" rel="shortcut icon">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Rubick admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Rubick Admin Template, dashboard template, flat admin template, responsive admin template, web app">
-    <meta name="author" content="LEFT4CODE">
+    <meta name="description" content="Web el Agite, Administrador Web">
+    <meta name="keywords" content="Web el Agite, Administrador Web">
+    <meta name="author" content="AFTER">
 
     @yield('head')
 
